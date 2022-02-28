26457ab4953a803fb283a776c6603c898c6c073c
vangona <rlarhksrud12@naver.com>
2022-02-28 15:28:48 +0900

    Add: 2022.02.28 2805번 나무자르기

---
+++ 2022.02.28/boj_2805.js
@@ -0,0 +1,35 @@
+// 2805번 나무 자르기
+
+// const fs = require('fs');
+// let [numbers, trees] = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+let [numbers, trees] = ['5 20', '4 42 40 26 46'];
+
+const [N, M] = numbers.split(' ').map(el => el * 1);
+trees = trees.split(' ').map(el => el * 1);
+
+let min = 1;
+let max = Math.max(...trees);
+let answer = 0;
+
+while(min < max) {
+    let mid = Math.floor((min + max) / 2);
+    let length = 0; 
+
+    trees.forEach(tree => {
+        if (tree - mid > 0) {
+            length += (tree - mid);
+        } 
+    })
+
+    if (length >= M) {
+        if (answer < mid) answer = mid;
+       min = mid + 1;
+    } else {
+        max = mid - 1;
+    }
+
+    length = 0;
+}
+
+console.log(answer);
