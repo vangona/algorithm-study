080d21dc6e01e35687962d64d94cd2b54ede363a
vangona <rlarhksrud12@naver.com>
2022-03-10 15:50:50 +0900

    2022.03.10 백준 1629번 곱셈

---
+++ 2022.03.10/boj_1629.js
@@ -0,0 +1,23 @@
+// 1629번 곱셈
+
+// const fs = require('fs');
+// const [A, B, C] = fs.readFileSync('/dev/stdin').toString().trim().split(' ').map(el => el * 1);
+
+const [A, B, C] = ['10', '11', '12'].map(el => BigInt(el));
+
+const answer = divideConquer(A, B, C);
+console.log(String(answer));
+
+function divideConquer(base, exponent, divider) {
+  if (base == 1) return 1;
+  if (exponent == 1) {
+    return base % divider;
+  } else {
+      const dividedNumber = divideConquer(base, BigInt(parseInt(exponent / BigInt(2))), divider);
+    if (exponent % BigInt(2) == 0) {    
+      return (dividedNumber * dividedNumber) % divider;
+    } else {
+      return (dividedNumber * dividedNumber * base) % divider;
+    }
+  }
+} 
